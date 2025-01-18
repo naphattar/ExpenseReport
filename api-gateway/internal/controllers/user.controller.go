40c33f4445b9a5b0b261f89c0b15911bd8b65af8
Naphat Chalermwai <naphatchalermwai@gmail.com>
2025-01-18 22:59:39 +0700

    [ADD] config auth routes for API Gateway

---
+++ api-gateway/internal/controllers/user.controller.go
@@ -0,0 +1,12 @@
+package controllers
+
+import(
+	"api-gateway/internal/config"
+	"github.com/gin-gonic/gin"
+)
+
+var User_Service_URL = 
+
+func RegisterHandler(c *gin.Context){
+
+}
---
+++ api-gateway/internal/utils/proxy.utils.go
@@ -0,0 +1,44 @@
+package utils
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func forwardRequest(c *gin.Context, method, url string) (*http.Response, error) {
+	var bodyBytes []byte
+	if c.Request.Body != nil {
+		bodyBytes, _ = io.ReadAll(c.Request.Body)
+	}
+	c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(bodyBytes))
+	if err != nil {
+		return nil, err
+	}
+
+	for key, values := range c.Request.Header {
+		for _, value := range values {
+			req.Header.Add(key, value)
+		}
+	}
+
+	for _, cookie := range c.Request.Cookies() {
+		req.AddCookie(cookie)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, cookie := range resp.Cookies() {
+		c.SetCookie(cookie.Name, cookie.Value, cookie.MaxAge, cookie.Path, cookie.Domain, cookie.Secure, cookie.HttpOnly)
+	}
+
+	return resp, nil
+}
