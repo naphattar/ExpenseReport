8fb3091ea8c2177479a7c24ae5f7e710dcebe274
Naphat Chalermwai <naphatchalermwai@gmail.com>
2025-01-20 18:17:51 +0700

    [ADD] add date in entry models

--- entry-service/internal/models/entry.go
+++ entry-service/internal/models/entry.go
@@ -1,22 +1,29 @@
 package models
 
 import (
-	"gorm.io/gorm"
 	"errors"
+	"time"
+
+	"gorm.io/gorm"
 )
-	
 
 type Entry struct {
 	gorm.Model
-	Type        string  `json:"type" gorm:"type:varchar(20);not null"`
-	Amount      float64 `json:"amount" gorm:"not null"`
-	Description string  `json:"description" gorm:"type:text"`
-	UserID      string  `json:"userId" gorm:"type:varchar(100);not null"`
+	Type        string    `json:"type" gorm:"type:varchar(20);not null"`
+	Amount      float64   `json:"amount" gorm:"not null"`
+	Description string    `json:"description" gorm:"type:text"`
+	UserID      string    `json:"userId" gorm:"type:varchar(100);not null"`
+	Date        time.Time `json:"date" gorm:"not null"`
 }
 
 func (e *Entry) Validate() error {
 	if e.Type != "Income" && e.Type != "Expense" {
 		return errors.New("type must be 'Income' or 'Expense'")
 	}
+
+	if e.Date.IsZero() {
+		return errors.New("date must be provided")
+	}
+
 	return nil
 }
